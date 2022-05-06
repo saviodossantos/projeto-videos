5c0ce163447e17543d5b8de259cd8252a6d20858
luna maria <lunaclark2002@gmail.com>
2022-05-06 09:44:35 -0300

    ALterações em login, contato e controle.js

--- js/controle.js
+++ js/controle.js
@@ -1,9 +1,14 @@
+var formC=document.forms.formContato
 document.querySelector("#btContato").onclick=function(){
     //alert('Aprende a escrever direito')
-    if(formC.emailUsuario.value == ""){
+    if(formC.nomeUsuario.value == ""){
+        alert("Preencha o nome!")
+     }else if (formC.emailUsuario.value == ""){
         alert("Preencha o email!")
     }else if (senhaUsuario.value == ""){
         alert("Preencha a senha!")
+    }else if (comentario.value == ""){
+            alert("Preencha o comentario!")
     }else{
-        formC.submit()
+        formC.submit()}
     }
