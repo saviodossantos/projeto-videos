d2a4b9ca6eb7d03e37d8af32ddd977059425dac7
carlinafaria <dev.carolinafaria@gmail.com>
2022-05-10 16:44:15 -0300

    refinamento da pagina produtos

--- js/promocoes.js
+++ js/promocoes.js
@@ -1,20 +1,66 @@
 console.log("Testando Js")
 
 var Super=[
-    ["Batman","filme1.jpeg", "Após dois anos espreitando as ruas como Batman, Bruce Wayne se encontra nas profundezas mais sombrias de Gotham City. Com poucos aliados confiáveis, o vigilante solitário se estabelece como a personificação da vingança para a população."],
-    ["Doutor Estranho", "filme2.jpeg" , "Em Doutor Estranho no Multiverso da Loucura, após derrotar Dormammu e enfrentar Thanos nos eventos de Vingadores: Ultimato, o Mago Supremo, Stephen Strange (Benedict Cumberbatch), e seu parceiro Wong (Benedict Wong), continuam suas pesquisas sobre a Joia do Tempo. Mas um velho amigo que virou inimigo coloca um ponto final nos seus planos e faz com que Strange desencadeie um mal indescritível, o obrigando a enfrentar uma nova e poderosa ameaça. O longa se conecta com a série do Disney+ WandaVision e tem relação também com Loki. O longa pertence a fase 4 do MCU onde a realidade do universo pode entrar em colapso por causa do mesmo feitiço que trouxe os vilões do Teioso para o mundo dos Vingadores e o Mago Supremo precisará contar com a ajuda de Wanda (Elizabeth Olsen), que vive isolada desde os eventos de WandaVision."], 
-    ["Matrix","filme3.jpeg" , "Se passando 20 anos após os acontecimentos de de Matrix Revolutions, Neo vive uma vida aparentemente comum sob sua identidade original como Thomas A. Anderson em São Francisco, Califórnia, com um terapeuta que lhe prescreve pílulas azuis para neutralizar as coisas estranhas e não naturais que ele ocasionalmente vislumbra em sua mente. Ele também conhece uma mulher que parece ser Trinity (Carrie Anne-Moss), mas nenhum deles se reconhece. No entanto, quando uma nova versão de Morpheus oferece a ele a pílula vermelha e reabre sua mente para o mundo da Matrix, que se tornou mais seguro e perigoso nos anos desde a infecção de Smith, Neo volta a se juntar a um grupo de rebeldes para lutar contra um novo e maois perigoso inimigo e livrar todos da Matrix novamente."], 
-    ["Morbius","filme4.jpeg" , "Baseado no personagem de mesmo nome da Marvel Comics, Michael Morbius (Jared Letto) sempre sofreu com uma condição rara em seu sangue que o faz andar de bengala e desde criança ser excluído por outros, mas sua vida solitária foi preenchida por livros. Após se formar na faculdade, Doutor Morbius é renomado na área de biomedicina e tenta achar uma cura para sua rara condição, afim de não apenas se ajudar, mas ajudar outras pessoas que também sofrem como ele. Experimentando com o DNA de morcegos, Morbius espera achar a cura e se usa como teste para o soro. Usando o DNA que isolou e uma mistura de eletrochoque, a cura foi um sucesso temporário, mas os efeitos colaterais acabaram transformando-o em um pseudo-vampiro e que agora precisa sobreviver como um. Apesar de ganhar capacidades iguais a de um morcego, Morbius precisa de sangue humano para sobreviver, os efeitos colaterais também o fez mudar fisicamente, ganhando presas e uma pele pálida. Além disso, a cada pessoa que ele morde, ela também vira um ser igual a ele."]
-]
+    ["Batman","filme1.jpeg", "Após dois anos espreitando as ruas como Batman, Bruce Wayne se encontra nas profundezas mais sombrias de Gotham City. Com poucos aliados confiáveis, o vigilante solitário se estabelece como a personificação da vingança para a população.","2022"," Ação/Fantasia"],
 
-console.log(Super)
+    ["Doutor Estranho 2", "filme2.jpeg" , "Em Doutor Estranho no Multiverso da Loucura, após derrotar Dormammu e enfrentar Thanos nos eventos de Vingadores: Ultimato, o Mago Supremo, Stephen Strange (Benedict Cumberbatch), e seu parceiro Wong (Benedict Wong), continuam suas pesquisas sobre a Joia do Tempo. Mas um velho amigo que virou inimigo coloca um ponto final nos seus planos e faz com que Strange desencadeie um mal indescritível, o obrigando a enfrentar uma nova e poderosa ameaça. O longa se conecta com a série do Disney+ WandaVision e tem relação também com Loki. O longa pertence a fase 4 do MCU onde a realidade do universo pode entrar em colapso por causa do mesmo feitiço que trouxe os vilões do Teioso para o mundo dos Vingadores e o Mago Supremo precisará contar com a ajuda de Wanda (Elizabeth Olsen), que vive isolada desde os eventos de WandaVision.","2022"," Ação/Aventura"], 
 
-var filmes = document.querySelector("#filmes")
+    ["Matrix","filme3.jpeg" , "Se passando 20 anos após os acontecimentos de de Matrix Revolutions, Neo vive uma vida aparentemente comum sob sua identidade original como Thomas A. Anderson em São Francisco, Califórnia, com um terapeuta que lhe prescreve pílulas azuis para neutralizar as coisas estranhas e não naturais que ele ocasionalmente vislumbra em sua mente. Ele também conhece uma mulher que parece ser Trinity (Carrie Anne-Moss), mas nenhum deles se reconhece. No entanto, quando uma nova versão de Morpheus oferece a ele a pílula vermelha e reabre sua mente para o mundo da Matrix, que se tornou mais seguro e perigoso nos anos desde a infecção de Smith, Neo volta a se juntar a um grupo de rebeldes para lutar contra um novo e maois perigoso inimigo e livrar todos da Matrix novamente.","202'"," Ação"], 
 
+    ["Morbius","filme4.jpeg" , "Baseado no personagem de mesmo nome da Marvel Comics, Michael Morbius (Jared Letto) sempre sofreu com uma condição rara em seu sangue que o faz andar de bengala e desde criança ser excluído por outros, mas sua vida solitária foi preenchida por livros. Após se formar na faculdade, Doutor Morbius é renomado na área de biomedicina e tenta achar uma cura para sua rara condição, afim de não apenas se ajudar, mas ajudar outras pessoas que também sofrem como ele. Experimentando com o DNA de morcegos, Morbius espera achar a cura e se usa como teste para o soro. Usando o DNA que isolou e uma mistura de eletrochoque, a cura foi um sucesso temporário, mas os efeitos colaterais acabaram transformando-o em um pseudo-vampiro e que agora precisa sobreviver como um. Apesar de ganhar capacidades iguais a de um morcego, Morbius precisa de sangue humano para sobreviver, os efeitos colaterais também o fez mudar fisicamente, ganhando presas e uma pele pálida. Além disso, a cada pessoa que ele morde, ela também vira um ser igual a ele.","2022"," Ação/Fantasia"],
+
+    ["Animais Fantásticos 2","filme5.jpeg","O professor Alvo Dumbledore (Jude Law) sabe que o poderoso mago das trevas Gellert Grindelwald (Mads Mikkelsen) está se movimentando para assumir o controle do mundo mágico. Incapaz de detê-lo sozinho, ele pede ao magizoologista Newt Scamander (Eddie Redmayne) para liderar uma intrépida equipe de bruxos, bruxas e um corajoso padeiro trouxa em uma missão perigosa, em que eles encontram velhos e novos animais fantásticos e entram em conflito com a crescente legião de seguidores de Grindelwald. Mas com tantas ameaças, quanto tempo poderá Dumbledore permanecer à margem do embate?","2022","Fantasia/aventura","2022"," Fantasia/Aventura"],
+
+    ["Sonic 2","filme6.jpeg","O Dr. Robotnik retorna à procura de uma esmeralda mística que tem o poder de destruir civilizações. Para detê-lo, Sonic se une a seu antigo parceiro, Tails, e parte em uma jornada para encontrar a joia antes que ela caia em mãos erradas.","2022", " Aventura/Comédia"],
+
+    ["Jungle Cruise","filme7.jpeg","A Dra. Lily Houghton pede a ajuda do sagaz capitão Frank Wolff para levá-la à Amazônia em seu barco em ruínas. Juntos, eles procuram por uma árvore ancestral que detém o poder de curar - uma descoberta que deve mudar o futuro da medicina.","2021"," Fantasia/Aventura"],
+
+    ["The Kings Man","filme8.jpeg","Os criminosos mais cruéis da história se reúnem para organizar uma guerra terrível para roubar milhões. No entanto, muito desastre pode acontecer se o plano for concretizado. Por isso, um corajoso homem precisa correr contra o tempo para detê-los.Ao se unir com um jovem, ele inicia uma parceria para acabar com as ameaças que assombram o mundo e o futuro da humanidade. Esta é a história da primeira agência de inteligência independente","2022"," Terror/Suspenso"],
+
+    ["Viúva Negra","filme9.jpeg","Ao nascer, a Viúva Negra, então conhecida como Natasha Romanova, é entregue à KGB, que a prepara para se tornar sua agente suprema. Porém, o seu próprio governo tenta matá-la quando a União Soviética se desfaz.","2021"," Ação" ],
+
+    ["Pixel","filme10.jpeg","Uma raça alienígena cria monstros inspirados em videogames da década de 1980 para conquistar a Terra, e cinco especialistas em jogos da época são convocados para combater o plano dos extraterrestres e salvar o planeta.","2015"," Animação"],
+
+    ["Homem-aranha: Sem volta para casa","filme11.jpeg","Pela primeira vez na história cinematográfica do Homem-Aranha, nosso herói amigo da vizinhança é desmascarado e não consegue mais separar sua vida normal dos grandes riscos de ser um super-herói. Quando ele pede ajuda ao Doutor Estranho, os riscos se tornam ainda mais perigosos, e o forçam a descobrir o que realmente significa ser o Homem-Aranha. Classificação indicativa 12 anos, contém violência.", "2021"," Ação/Aventura"],
+
+    ["Tô rica! 2","filme12.jpeg","Uma estranha de mesmo nome de Selminha se diz herdeira legítima de sua fortuna. Com isso, os bens dela são congelados e sua única fonte de renda passa a ser 30 reais por dia. Agora, ela precisa voltar às origens e sobreviver em sua nova rotina.","2022"," comédia"],
+
+    ["Velozes & Furiosos 9","filme13.jpeg","Quando se tornou pai, Dominic Toretto (Vin Diesel) tentou deixar o eletrizante mundo de corridas de lado, tentando proteger seu filho de todos os perigos do passado. No entanto, não importa o quanto ele tente, as questões pendentes sempre voltam à tona. Agora Dom pretende reunir novamente seu grupo para venceu o mal que se aproxima. Enquanto isso, Cipher (Charlize Theron) pede ajuda a Jakob Toretto (John Cena) para acabar com Dominic de uma vez por todas.","2021"," Ação/aventura"],
+
+    [" Como eu era antes de você","filme14.jpeg","De origem modesta e sem grandes aspirações, a peculiar Louisa Clark é contratada para ser cuidadora de Will, um jovem tetraplégico depressivo e cínico.","2016"," Romance"],
+
+    ["Mercenários 3","filme15.jpeg","Barney e sua trupe de mercenários resgatam Doc, um dos integrantes do grupo, que estava preso há oito anos. Participando de uma missão, os mercenários são surpreendidos quando reencontram Conrad, um comerciante de armas impiedoso, que Barney acreditava ter matado. Novos membros, mais jovens e mais rápidos, entram para equipe e um novo duelo começa.","2014"," ação"],
+
+    ["Simplesmente Acontece","filme16.jpeg","Alex e Rosie são amigos inseparáveis que cresceram juntos em Londres, compartilhando entre si suas melhores experiências. Tudo muda quando Alex ganha uma bolsa de estudos e passa a morar nos EUA. Separados, seus caminhos agora são outros. Mas nos tempos de hoje é impossível não se conectar. E, em se tratando de amor, o difícil é fazer as escolhas certas.","2014"," Comédia Romantica"],
+
+    ["A menina que roubava livros","filme17.jpeg", "Durante a Segunda Guerra Mundial, uma jovem garota chamada Liesel Meminger sobrevive fora de Munique lendo os livros que ela rouba. Ajudada por seu pai adotivo, ela aprende a ler e partilhar livros com seus amigos, incluindo um judeu que vive na clandestinidade em sua casa. Enquanto não está lendo ou estudando, ela faz algumas tarefas para a mãe e brinca com o amigo Rudy.","2013"," Drama"],
+
+    ["Deadpool 2","filme18.jpeg","O mercenário mutante de boca suja Wade Wilson (também conhecido como Deadpool) reúne uma equipe de companheiros mutantes desonestos para proteger um jovem com habilidades sobrenaturais do brutal e viajante do tempo do cabo ciborgue.","2016"," Comédia/Ação"],
+
+    ["Mulan","filme19.jpeg","Para salvar seu pai doente de servir no Exército Imperial, uma jovem destemida se disfarça de homem para lutar contra os invasores do norte da China.","2020"," Live Action"],
+
+    ["Aquaman","filme20.jpeg","'Aquaman' se centrará em Aquaman como um governante relutante do reino subaquático da Atlântida, preso entre os habitantes da terra que sempre polupem o globo e seu próprio povo que está pronto para invadir a superfície. Uma segunda variante pode ser encontrada na parte inferior da página: Aquaman é o rei dos sete mares.","2018"," Ação/Aventura"]]
+    
+var filmes=document.querySelector("#filmes")
+//logica de iteração para consumo do array
 for(var i=0;i<Super.length;i++){
-    var img = new Image()
-    img.src = "imgs/" + Super[i][1]
-    img.width = 150
-    filmes.innerHTML += "<p>" + Super[i][0] + "</p>" + "<p>" + Super[i][2] + "</p>"
-    filmes.appendChild(img)
-}
+   // cria um elemento div
+   var div=document.createElement('div')
+   //adiciona a classe com espaço 6
+   div.classList.add('col-sm-4')
+   div.classList.add('mt-3')
+   var conteudo=document.createElement('div')
+   conteudo.className='row'
+
+    //insere o tag h3 com o titulo
+    conteudo.innerHTML+= '<h3 class="col-sm-12">' + Super[i][0]+ '</h3>'
+    //insere a imagem com 100% dentro de uma div espaço 3
+    conteudo.innerHTML+= '<div class="col-sm-8"><img src="imgs/' + Super[i][1]+ '" class="w-100"/></div>'
+    //insere o paragrafo com a resenha dentro de uma div espaço 9
+    conteudo.innerHTML+='<div class="col-sm-8"><p> '+ Super[i][2] +' </p></div>' 
+    conteudo.innerHTML+=' <div class="col-sm-8"><p> '+ Super[i][3] +  Super[i][4] + ' </p></div>'
+   // conteudo.innerHTML+='<div class="col-sm-8"><p>'+ Super[i][4] +'</p></div>'
+    //div recebe conteudo
+    div.appendChild(conteudo)
+    //galeria recebe div
+    filmes.appendChild(div)}
