e9a2e4f425dc604801b358213e64c3bb7f65ba0b
Luiz Eduardo  Fernandes <79099954+Lui-z@users.noreply.github.com>
2022-05-12 15:31:36 -0300

    Index com botão novo em entrar e sair + index2.js

--- js/login.js
+++ js/login.js
@@ -9,5 +9,6 @@ document.querySelector("#btLogin").onclick=function(){
         alert("Preencha a senha!")
     }else{
         formL.submit()
+        localStorage.emailUser=formL.email.value
     }
 }
