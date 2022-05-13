eee886d1f0b9acef1e6f74aa3a7e37094ce86cee
Sávio dos Santos <s.santos.carneiro@accenture.com>
2022-05-13 17:50:20 -0300

    ajuste na tabela do carrinho e logica do resumo do pedido parcialmente feita

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,40 +1,26 @@
 let carrinho = [
-    '{"produto":["filme1.jpeg","filme2.jpeg","filme3.jpg"],"qtd":"100","preco":["R$10,90","R$29,90","R$7,33"]}'
+    '{"produto":["filme1.jpeg","filme2.jpeg","filme3.jpeg"],"qtd":["1","2","3"],"preco":[20,30,10]}'
 ]
-let carrinhoJSON = JSON.parse(carrinho) //JSON.parse transforma o array usuario em objeto
-let listaCarrinho = document.querySelector("#listaCarrinho")
-let td
-console.log(carrinho)
-console.log(carrinhoJSON)
+let carrinhoJSON = JSON.parse(carrinho)
+
+let listCarrinho = document.querySelector("#listaCarrinho")
+
+let valor = document.querySelector("#valorTotal")
 
 for (var i = 0; i < carrinhoJSON.produto.length; i++) {
-    
-    let produtoCarrinho = document.querySelector("#produtoCarrinho")
-    produtoCarrinho.innerHTML += '<img src="../imgs/' + carrinhoJSON.produto[i] + '">'
-    // produtoCarrinho.classList.add('w-50')
+    var tr = document.createElement('tr')
 
+    tr.innerHTML += '<td style="vertical-align:unset;">' + '<img class="w-25 m-1 imagemCarrinho" src="../imgs/' + carrinhoJSON.produto[i] + '"></td>'
+    tr.innerHTML += '<td style="vertical-align:unset;">' + carrinhoJSON.qtd[i] + '</td>'
+    tr.innerHTML += '<td style="vertical-align:unset;">' + carrinhoJSON.preco[i] + '</td>'
+
+    listaCarrinho.appendChild(tr)
+    
 }
 
-// for (var i = 0; i < carrinhoJSON.qtd.length; i++) {
-    
-//     let qtdCarrinho = document.querySelector("#qtdCarrinho")
-//     qtdCarrinho.innerHTML += '<td>' + carrinhoJSON.qtd[i] + '</td>'
+var val1 = carrinhoJSON.preco[0];
+var val2 = carrinhoJSON.preco[1];
+var val3 = carrinhoJSON.preco[2];
+var valorTotal = parseInt(val1) + parseInt(val2) + parseInt(val3);
+valor.innerHTML = valorTotal
 
-// }
-
-// for (var i = 0; i < carrinhoJSON.preco.length; i++) {
-    
-//     let precoCarrinho = document.querySelector("#preco")
-//     precoCarrinho.innerHTML += '<td>' + carrinhoJSON.preco[i] + '</td>'
-
-// }
-
-listaCarrinho.appendChild("#produtoCarrinho")
-
-
-
-// let livrosFor = document.querySelector("#livrosFor")
-
-// for(var i=0; i < usuarioJSON.livros.length; i++){
-//     livrosFor.innerHTML += (i+1) + ': ' + usuarioJSON.livros[i] + '<br>'
-// }
