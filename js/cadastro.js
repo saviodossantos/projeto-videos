92fcd51b8f4cbdfc465891e61d92241653b549d0
Grazielle M. Santiago <104645740+gramedsant@users.noreply.github.com>
2022-05-13 15:23:11 -0300

    remoção senha e confirmação de senha do localStorage e refinamento do campo 'telefone' da página Cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -2,23 +2,28 @@ console.log(document.forms.formContato)
 var formC = document.forms.formContato
 
 document.querySelector("#btContato").onclick=function(){
+    function mascaraDeTelefone(  ){
+        const textoAtual = telefone.value;
+    }
     
 if(formC.nomeContato.value == ""){
     alert("Preencha o seu nome!")
 }else if (formC.emailContato.value == ""){
     alert("Preencha o seu e-mail!")
-}else if (formC.telefoneContato.value == ""){
+}else if (formC.telefoneContato.value == "( )"){
     alert("Preencha o seu telefone!")
 }else if (formC.senhaContato.value == ""){
     alert("Preencha a sua senha!")
 }else if (formC.confsenhaContato.value == ""){
-    alert("Confirme a sua senha!")
+    alert("Confirme a sua senha!")       
+        }else if(formC.confsenhaContato.value !== formC.senhaContato.value){
+            alert("A senha não confere!")
 }else{
     formC.submit()
-    localStorage.nomeContato=formC.nomeContato.value
-    localStorage.emailContato=formC.emailContato.value
-    localStorage.telefoneContato=formC.telefoneContato.value
-    localStorage.senhaContato=formC.senhaContato.value
-    localStorage.confsenhaContato=formC.confsenhaContato.value      
+    localStorage.Nome=formC.nomeContato.value
+    localStorage.Email=formC.emailContato.value
+    localStorage.Telefone=formC.telefoneContato.value
+    localStorage.removeItem=senhaContato
+    localStorage.removeItem=confsenhaContato     
 }
 }
