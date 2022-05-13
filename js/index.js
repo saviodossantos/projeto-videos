56b546eaa3865d29faf49e15432d675f58deca6f
Savio dos Santos <savio.dossantos@outlook.com>
2022-05-12 21:05:21 -0300

    fix padronizacao de header, footer e organizacao de pastas

--- js/index.js
+++ js/index.js
@@ -13,18 +13,16 @@ var videos = [
 
 var galeriaVideos = document.querySelector("#galeriaVideos") //var galeria acessa div com id galeria
 
-for (var i=0; i < videos.length; i++){ //lógica de iteração para consumo do array
-    
+for (var i = 0; i < videos.length; i++) { //lógica de iteração para consumo do array
+
     var single = 'single.html'
 
     var conteudo = document.createElement('div')
     conteudo.classList.add('col-sm-4')
     conteudo.classList.add('mt-5')
-    
+
     conteudo.innerHTML += '<a href="docs/' + single + '"><img src="imgs/' + videos[i] + '" class="imagens"></a>'
     galeriaVideos.appendChild(conteudo)
-    
+
     console.log(videos)
 }
-
-
