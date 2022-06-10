412c46de0af5a74985d4733d802f50887ab54aea
luna maria <lunaclark2002@gmail.com>
2022-06-10 14:54:39 -0300

    index_ejs.js

--- index_ejs.js
+++ index_ejs.js
@@ -1,4 +1,5 @@
 (async () => {
+    const url = require("url")
 const express = require('express')
 const app = express()
 const db = require("./db.js")
@@ -7,18 +8,26 @@ const port = 3000
 app.set("view engine","ejs")
 
 
+
+
+
 app.use(express.static('projeto-videos'))
 app.use("/imgs",express.static("imgs"))
 app.use("/js",express.static("js"))
 app.use("/css",express.static("css"))
 
 const consulta = await db.selectFilmes()
-    console.log(consulta[0])
+    console.log(consulta[1])
+    const consultaFilmes = await db.selectFilmes()
+    //console.log(consultaFilmes[0])
 
 
 //index (Quando criar o html diretamente no res.end() insira o writeHead)
 app.get("/",(req,res)=>{
+   // filmes:consulta,
+   //  filmes:consultaPromo
     res.render(`index`,{promo:"Todos os filmes com 10% de desconto!"})
+     
 })
 
 app.get("/produtos",(req,res)=>{
@@ -37,10 +46,25 @@ app.get("/cadastro",(req,res)=>{
     res.render(`single`)
  })
 
- app.get("/promocoes",(req,res)=>{
-    res.render(`promocoes`,{filmes:consulta})
+ app.get("/promocoes",async(req,res)=>{
+    consultaPromo = await  db.selectPromo()
+    res.render(`promocoes`,{
+      filmes:consulta,
+      filmes:consultaPromo
+    })
  })
-
+ app.get("/atualiza-promo",async(req,res)=>{
+    let qs = url.parse(req.url,true).query
+    await db.updatePromo(qs.promo,qs.id)//localhost:3000/atualiza-promo?promo=1&id=1
+        res.send("<h2>Lista de promoções atualizada!</h2><a href='./promocoes'>Voltar</a>")
+})
+  
+ app.get("/upd-promo",async(req,res)=>{
+    res.render(`adm/atualiza-promocoes`,{
+       filmes:consulta,
+      filmes:consultaFilmes
+    })
+})
  app.listen(port,() => console.log(`Servidor rodando na porta ${port} - ${__dirname}`))
 })()
 
