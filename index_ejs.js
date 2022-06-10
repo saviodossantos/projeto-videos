a8e6d02d3b067f85f29850b807fff2e1bf065f5e
Sávio dos Santos <savio.dossantos@outlook.com>
2022-06-10 16:16:38 -0300

    carrinho ejs

--- index_ejs.js
+++ index_ejs.js
@@ -1,46 +1,55 @@
 (async () => {
-const express = require('express')
-const app = express()
-const db = require("./db.js")
-const port = 3000
+    const express = require('express')
+    const app = express()
+    const db = require("./db.js")
+    const port = 3000
 
-app.set("view engine","ejs")
-
-
-app.use(express.static('projeto-videos'))
-app.use("/imgs",express.static("imgs"))
-app.use("/js",express.static("js"))
-app.use("/css",express.static("css"))
-
-const consulta = await db.selectFilmes()
-    console.log(consulta[0])
+    app.set("view engine", "ejs")
 
 
-//index (Quando criar o html diretamente no res.end() insira o writeHead)
-app.get("/",(req,res)=>{
-    res.render(`index`,{promo:"Todos os filmes com 10% de desconto!"})
-})
+    app.use(express.static('projeto-videos'))
+    app.use("/imgs", express.static("imgs"))
+    app.use("/js", express.static("js"))
+    app.use("/css", express.static("css"))
 
-app.get("/produtos",(req,res)=>{
-    res.render(`produtos`)
-})
+    const consulta = await db.selectFilmes()
+    const consultaCarrinho = await db.selectCarrinho()
 
-app.get("/contato",(req,res)=>{
-   res.render(`contato`)
-})
+    //index (Quando criar o html diretamente no res.end() insira o writeHead)
+    app.get("/", (req, res) => {
+        res.render(`index`, {
+            promo: "Todos os filmes com 10% de desconto!"
+        })
+    })
 
-app.get("/cadastro",(req,res)=>{
-    res.render(`cadastro`)
- })
+    app.get("/produtos", (req, res) => {
+        res.render(`produtos`)
+    })
 
- app.get("/single",(req,res)=>{
-    res.render(`single`)
- })
+    app.get("/contato", (req, res) => {
+        res.render(`contato`)
+    })
 
- app.get("/promocoes",(req,res)=>{
-    res.render(`promocoes`,{filmes:consulta})
- })
+    app.get("/cadastro", (req, res) => {
+        res.render(`cadastro`)
+    })
 
- app.listen(port,() => console.log(`Servidor rodando na porta ${port} - ${__dirname}`))
+    app.get("/single", (req, res) => {
+        res.render(`single`)
+    })
+
+    app.get("/promocoes", (req, res) => {
+        res.render(`promocoes`, {
+            filmes: consulta
+        })
+    })
+
+    app.get("/carrinho", (req, res) => {
+        res.render(`carrinho`, {
+            carrinho: consultaCarrinho
+        })
+    })
+
+    app.listen(port, () => console.log(`Servidor rodando na porta ${port} - ${__dirname}`))
 })()
 
