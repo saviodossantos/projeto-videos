3acd4832abec1c7226cf7253956e4f0bd839ebbe
luna maria <lunaclark2002@gmail.com>
2022-06-20 14:48:42 -0300

    Alteração na pagina Cadastro de produto

--- index_ejs.js
+++ index_ejs.js
@@ -84,115 +84,181 @@ function checkFirst(req, res, next) {
 
  })
 
-    const express = require('express')
-        const app = express()
-        const db = require("./db.js")
-        const bodyParser = require("body-parser")
-        const url=require("url")
-        const port = 3000
-        const session = require("express-session")
-        
-        app.set("view engine","ejs")
-        
-        app.use(bodyParser.urlencoded({extended:false}))
-        app.use(bodyParser.json())
 
-        app.use(express.static('projeto-videos'))
-        app.use("/imgs",express.static("imgs"))
-        app.use("/js",express.static("js"))
-        app.use("/css",express.static("css"))
-        
-        const consulta = await db.selectFilmes()
-        const consultaCarrinho = await db.selectCarrinho()
-        const consultaFilmes = await db.selectFilmes()
-    
-        //index (Quando criar o html diretamente no res.end() insira o writeHead)
-        app.get("/",(req,res)=>{
-            res.render(`index`,{promo:"Todos os filmes com 10% de desconto!"})
-        })
-        
-        app.get("/produtos",(req,res)=>{
-            res.render(`produtos`)
-        })
-        
-        app.get("/contato",(req,res)=>{
-           res.render(`contato`)
-        })
-        
-        app.get("/cadastro",async(req,res)=>{
-            let infoUrl=req.url
-            let urlProp=url.parse(infoUrl,true)
-            let q=urlProp.query
-           
-            res.render(`cadastro`,{filmes:consulta,
-                galeria:consultaFilmes
-            })
-
-          })
-        app.get("/single",(req,res)=>{
-        res.render(`single`)
-        })
-        
-        app.get("/login",(req,res)=>{
-            res.render(`login`)
-         })
-    
-        app.get("/carrinho", (req, res) => {
-         res.render(`carrinho`, {
-            carrinho: consultaCarrinho
-         })
-        })
-        
-        app.get("/promocoes",async(req,res)=>{
-           const consultaPromo=await db.selectPromo()
-            res.render(`promocoes`,{
-                filmes:consulta,
-            galeria:consultaPromo})
-         })
-       
-         app.get("/single",async(req,res)=>{
-             let infoUrl=req.url
-             let urlProp=url.parse(infoUrl,true)
-             let q=urlProp.query
-             const consultaSingle=await db.selectSingle(q.id)
-             const consultaInit=await db.selectSingle(4)
-            res.render(`single`,{filmes:consulta,
-                galeria:consultaSingle,
-            inicio:consultaInit})
-         })
-        
-         app.get("/atualiza-promo",async(req,res)=>{
-            let qs = url.parse(req.url,true).query
-            await db.updatePromo(qs.promo,qs.id)//localhost:3000/atualiza-promo?promo=1&id=1
-                res.send("<h2>Lista de promoções atualizada!</h2><a href='./promocoes'>Voltar</a>")
-        })
-          
-         app.get("/upd-promo",async(req,res)=>{
-            res.render(`adm/atualiza-promocoes`,{
-               filmes:consulta,
-              filmes:consultaFilmes
-            })
-
-        app.post("/cadastro",async(req,res)=>{
-          const info=req.body
-         await db.insertUsuarios({
-            nome:info.nome,
-            email:info.email,
-            telefone:info.telefone,
-            senha:info.senha,
-            conf_senha:info.conf_senha
-           })
-            res.redirect(`/`)
-            })
-        app.post("/contato",async(req,res)=>{
-        const info=req.body
-         await db.insertContato({
-           nome:info.nomeContato,
-        email:info.cad_emailContato,
-        mensagem:info.cad_mensagem})
-        res.redirect("/promocoes")
+   app.get("/", async (req, res) => {
+      res.render(`index`, {
+         titulo: " Conheça os nossos Filmes",
+         promo: " Todos os Filmes com 10% de desconto !",
+         filmes: consulta,
+         galeria: consultaFilmes
       })
-         })
-         app.listen(port,() => console.log(`Servidor rodando na porta ${port} - ${__dirname}`))
-        
-        })()
+   })
+
+  app.get("/produtos", (req, res) => {
+      res.render(`produtos`, {
+         galeria: consultaFilmes
+      })
+   })
+
+   app.get("/contato",async(req,res)=>{
+      let infoUrl=req.url
+      let urlProp= url.parse(infoUrl,true)
+      let q =urlProp.query
+      res.render(`contato`,{
+      filmes:consulta,
+      galeria:consultaFilmes
+    })
+   })
+    app.post("/contato",async(req,res)=>{
+      const info=req.body
+       await db.insertContato({
+         nome:info.cad_nome,
+      sobrenome:info.cad_sobrenome,
+      email:info.cad_email,
+      mensagem:info.cad_mensagem})
+      res.redirect("/promocoes")
+    })
+   
+    app.get("/cadastro",async(req,res)=>{
+      let infoUrl=req.url
+      let urlProp= url.parse(infoUrl,true)
+      let q =urlProp.query
+      const consultaSingle = await db.selectSingle(q.id)
+      const consultaInit =await db.selectSingle(4)
+      res.render(`cadastro`,{
+      filme:consulta,
+      galeria:consultaInit
+    })
+    })
+    app.post("/cadastro",async(req,res)=>{
+      const info=req.body
+       await db.cadastroContato({
+      nome:info.nome,
+      email:info.email,
+      telefone:info.telefone,
+      senha:info.senha,
+      conf_senha:info.conf_senha})
+      res.redirect("/login")
+    })
+    
+
+   app.get("/carrinho", (req, res) => {
+      res.render(`carrinho`, {
+         carrinho: consultaCarrinho
+      })
+   })
+
+   app.get("/promocoes", async (req, res) => {
+      const consultaPromo = await db.selectPromo()
+      res.render(`promocoes`, {
+         filmes: consulta,
+         galeria: consultaPromo
+      })
+   })
+
+   app.get("/single", async (req, res) => {
+      let infoUrl = req.url
+      let urlProp = url.parse(infoUrl, true)
+      let q = urlProp.query
+      const consultaSingle = await db.selectSingle(q.id)
+      const consultaInit = await db.selectSingle(4)
+      res.render(`single`, {
+         filmes: consulta,
+         galeria: consultaSingle,
+         inicio: consultaInit
+      })
+   })
+
+   app.get("/atualiza-promo", async (req, res) => {
+      let qs = url.parse(req.url, true).query
+      await db.updatePromo(qs.promo, qs.id)//localhost:3000/atualiza-promo?promo=1&id=1
+      res.send("<h2>Lista de promoções atualizada!</h2><a href='./promocoes'>Voltar</a>")
+   })
+
+   app.get("/upd-promo", async (req, res) => {
+      res.render(`adm/atualiza-promocoes`, {
+         filmes: consulta,
+         filmes: consultaFilmes
+      })
+   })
+   app.get("/produtos",async(req,res)=>{
+      const consultaProduto= await db.selectFilmes()
+     res.render(`produtos`,{
+         galeria:consultaProduto
+     })
+ })
+ app.get("/adm", async (req, res) => {
+   res.render(`adm/index-admin`, {
+      titulo: "login do Adm",      
+      filmes: consulta,
+      galeria: consultaFilmes
+   })
+})
+app.get("/adm/cadastroAdm",async(req,res)=>{
+   let infoUrl=req.url
+   let urlProp= url.parse(infoUrl,true)
+   let q =urlProp.query   
+   res.render(`adm/cadastroAdm`,{
+ 
+ })
+ })
+ app.post("/adm/cadastroAdm",async(req,res)=>{
+   const info=req.body
+   await db.cadastroAdm({
+   nome:info.nome,
+   email:info.email,
+   telefone:info.telefone,
+   senha:info.senha,
+   confsenha:info.confsenha})
+   res.redirect(`/adm`)
+ })
+
+ app.get("/adm/login-admin",async(req,res) => {
+    res.render(`adm/login-admin`,{
+})
+ })
+
+ app.post("/adm/login-admin", async (req,res)=>{
+   const {email,senha} = req.body
+   const logado = await db.selectAdm(email,senha)
+   if(logado != ""){
+   req.session.userInfo = email
+   userInfo = req.session.userInfo
+   req.app.locals.info.user= userInfo
+   res.redirect('/adm')
+   } else {res.send("<h2>Login ou senha não conferem</h2>")}
+})
+app.use('/logout', function (req,res) {
+req.app.locals.info = {}
+req.session.destroy()
+res.clearCookie('connect.sid', { path: '/' });
+res.redirect(`adm/login-adm`) 
+})
+
+
+app.get("/adm/cadastroProduto",async(req,res)=>{
+   let infoUrl=req.url
+   let urlProp= url.parse(infoUrl,true)
+   let q =urlProp.query
+   res.render(`adm/cadastroProduto`,{
+   
+ })
+ }) 
+ app.post("/adm/cadastroProduto",async(req,res)=>{
+   const info=req.body
+   await db.insertFilmes({
+   titulo:info.titulo,
+   genero:info.genero,
+   Ano:info.Ano,
+   sinopse:info.sinopse,
+   imagem:info.imagem,
+   promo:info.promo,
+   valor:info.valor})
+
+   res.redirect(`/adm`)
+ })
+
+
+   app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
+
+})()
